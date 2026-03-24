39eff5f2bba056ccf5976d21a894b4ea62f9eaaf
Patcharadnai <patcharadnai@MacBook-Air.local>
2026-03-25 02:38:04 +0700

    fix server ready for deploy

--- src/app.js
+++ src/app.js
@@ -1,39 +1,44 @@
 require("dotenv").config();
+
 const express = require('express');
+const cors = require('cors');
+const app = express();
 app.use(cors({
     origin: [
         "https://patcharadnaimingchua.github.io"
     ],
     credentials: true
 }));
-const app = express();
-const authRoutes = require('./routes/auth.routes')
-const productRoutes = require('./routes/product.routes')
-const categoryRoutes = require('./routes/category.routes')
-const cartRoutes = require('./routes/cart.routes')
-const orderRoutes = require('./routes/order.routes')
-const paymentRoutes = require('./routes/payment')
-const reviewRoutes = require('./routes/review.routes')
 
-app.use(cors())
-app.use(express.json())
-app.use('/auth', authRoutes)
-app.use('/products', productRoutes)
-app.use('/categories', categoryRoutes)
-app.use('/cart', cartRoutes)
-app.use('/orders', orderRoutes)
-app.use('/payments', paymentRoutes)
-app.use('/reviews', reviewRoutes)
+app.use(express.json());
+
+const authRoutes = require('./routes/auth.routes');
+const productRoutes = require('./routes/product.routes');
+const categoryRoutes = require('./routes/category.routes');
+const cartRoutes = require('./routes/cart.routes');
+const orderRoutes = require('./routes/order.routes');
+const paymentRoutes = require('./routes/payment');
+const reviewRoutes = require('./routes/review.routes');
+
+app.use('/auth', authRoutes);
+app.use('/products', productRoutes);
+app.use('/categories', categoryRoutes);
+app.use('/cart', cartRoutes);
+app.use('/orders', orderRoutes);
+app.use('/payments', paymentRoutes);
+app.use('/reviews', reviewRoutes);
+
 app.get("/", (req, res) => {
-    res.send("Ecommerce API running")
-})
+    res.send("Ecommerce API running");
+});
+
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).json({ message: "Internal Server Error" });
+});
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-});
-app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).json({ message: "Internal Server Error" });
 });
